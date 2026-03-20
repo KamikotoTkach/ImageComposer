89f5e9c4f8bbc383d86b752b490aaaea1ca6310a
Danil Tkach <tkach@cwcode.ru>
2026-03-20 13:53:50 +0300

    add timeouts for telegram notifier

--- src/main/java/ru/cwcode/tkach/imagecomposer/data/notify/TelegramNotify.java
+++ src/main/java/ru/cwcode/tkach/imagecomposer/data/notify/TelegramNotify.java
@@ -9,6 +9,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.List;
 
 public class TelegramNotify {
@@ -27,6 +28,7 @@ public class TelegramNotify {
     HttpClient client = proxyEnabled
       ? HttpClient.newBuilder()
                   .proxy(ProxySelector.of(new InetSocketAddress(proxyHost, proxyPort)))
+                  .connectTimeout(Duration.ofMillis(3000))
                   .build()
       : HttpClient.newHttpClient();
     
@@ -40,6 +42,7 @@ public class TelegramNotify {
                          .uri(java.net.URI.create("https://api.telegram.org/bot" + token + "/sendMessage"))
                          .header("Content-Type", "application/x-www-form-urlencoded")
                          .POST(body)
+                         .timeout(Duration.ofMillis(10000))
                          .build();
     
     client.send(req, HttpResponse.BodyHandlers.ofString());
